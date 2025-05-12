0f853ce5596219b77058498edd876d1b55a14bc2
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2025-05-12 13:35:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 conn = st.connection("my_example_connection")
-df = conn.query("SELECT * FROM customer_reviews")
+df = conn.query("SELECT * FROM avalanche_db.public.customer_reviews")
 df
 
 st.title('🎈 App Name')
