5c270c1ef010b199a998fe63abc781a5734d16d9
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2025-05-12 13:45:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,7 @@ import snowflake
 
 st.title('🎈 App Name')
 
-conn = snowflake.connector.connect(
-    user=st.secrets['user'],
-    password=st.secrets['password'],
-    account=st.secrets['account'],
-    warehouse=st.secrets['warehouse'],
-    database=st.secrets['database'],
-    schema=st.secrets['schema']
-)
-
-df = conn.query("SELECT * FROM avalanche_db.public.customer_reviews")
+df = conn.query("SELECT * FROM avalanche_db.public.customer_reviews;")
 df
 
 
