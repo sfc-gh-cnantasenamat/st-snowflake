fd6df26d0d7693fb201033017b4c23fd43943e04
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2025-05-14 14:23:01 -0700

    Update cortex.py

--- cortex.py
+++ cortex.py
@@ -9,6 +9,6 @@ df
 
 prompt = st.text_input('What do you want to know?', placeholder='Ask a question')
 if st.button("Submit", type='primary'):
-  response = conn.query(f"SELECT SNOWFLAKE.CORTEX.COMPLETE('claude-3-5-sonnet', {prompt}) as RESPONSE;")
+  response = conn.query("SELECT SNOWFLAKE.CORTEX.COMPLETE('claude-3-5-sonnet', f'{prompt}') as RESPONSE;")
   response_value = response.loc[0, 'RESPONSE']
   st.markdown(response_value)
