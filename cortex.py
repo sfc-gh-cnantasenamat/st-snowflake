126568ec847b523eb39ea05f97db5f1f89864edb
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2025-05-14 11:44:27 -0700

    Update cortex.py

--- cortex.py
+++ cortex.py
@@ -1,9 +1,24 @@
-import streamlit as st
+# import streamlit as st
+# from snowflake.snowpark.context import get_active_session
+
+# session = get_active_session()
+
+# st.title("🤖 Snowflake Cortex")
+
+# response = session.sql("SELECT SNOWFLAKE.CORTEX.COMPLETE('claude-3-5-sonnet', 'What is Python?') as RESPONSE;")
+# st.write(response)
+
+from snowflake.core import Root
 from snowflake.snowpark.context import get_active_session
 
-session = get_active_session()
+def embed_service():
+    # Initialize Snowflake session and root
+    session = get_active_session()
+    root = Root(session)
 
-st.title("🤖 Snowflake Cortex")
+    # Send embed_request request and process response
+    response = root.cortex_embed_service.embed("e5-base-v2", ['foo', 'bar'])
+    print(response)
 
-response = session.sql("SELECT SNOWFLAKE.CORTEX.COMPLETE('claude-3-5-sonnet', 'What is Python?') as RESPONSE;")
-st.write(response)
+if __name__ == "__main__":
+    embed_service()
