5d27d1c3731c54e69f672a69e9e742da3f1ac0cc
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2025-05-14 10:46:52 -0700

    Create cortex.py

---
+++ cortex.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from snowflake.cortex import complete
+
+st.title("🤖 Snowflake Cortex")
+
+code = 'complete('claude-3-5-sonnet',"What is Python?")'
+st.code(code)
+
+response = complete('claude-3-5-sonnet',"What is Python?")
+st.write(response)
