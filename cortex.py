08a623e53051c0516d357c7bca0b496f5c476bce
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2025-05-14 15:06:39 -0700

    Update cortex.py

--- cortex.py
+++ cortex.py
@@ -40,7 +40,7 @@ with tabs[1]:
   if st.button("Submit", type='primary'):
     response = conn.query(f"SELECT SNOWFLAKE.CORTEX.COMPLETE('claude-3-5-sonnet', '{prompt}') as RESPONSE;")
     response_value = response.loc[0, 'RESPONSE']
-    st.markdown(response_value)
+    st.code(response_value)
 
   with st.expander("Show Code"):
     st.code(code_1)
