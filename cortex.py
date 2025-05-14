b1522fe989a41990a7df9ad72cd4118b1f93375b
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2025-05-14 15:03:52 -0700

    Update cortex.py

--- cortex.py
+++ cortex.py
@@ -20,18 +20,20 @@ code_1 = """
 """
 
 with tabs[0]:
-  st.code(code_0)
-  
   conn = st.connection("snowflake")
   df = conn.query("SELECT * FROM avalanche_db.public.customer_reviews;")
   df
 
-with tabs[1]:
-  st.code(code_1)
+  with st.expander("Show Code"):
+    st.code(code_0)
   
+with tabs[1]:
   prompt = st.text_input('What do you want to know?', placeholder='Ask a question')
   
   if st.button("Submit", type='primary'):
     response = conn.query(f"SELECT SNOWFLAKE.CORTEX.COMPLETE('claude-3-5-sonnet', '{prompt}') as RESPONSE;")
     response_value = response.loc[0, 'RESPONSE']
     st.markdown(response_value)
+
+  with st.expander("Show Code"):
+    st.code(code_1)
