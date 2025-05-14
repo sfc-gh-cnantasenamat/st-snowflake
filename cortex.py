ef68c8ec3781157da979bc1633fa8abad7099692
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2025-05-14 10:55:22 -0700

    Update cortex.py

--- cortex.py
+++ cortex.py
@@ -3,7 +3,7 @@ from snowflake.cortex import complete
 
 st.title("🤖 Snowflake Cortex")
 
-code = 'complete('claude-3-5-sonnet',"What is Python?")'
+code = 'complete("claude-3-5-sonnet", "What is Python?")'
 st.code(code)
 
 response = complete('claude-3-5-sonnet',"What is Python?")
